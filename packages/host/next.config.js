128f4b0c553483944c50cd8f3f4e373e0802da6a
suum-lion <suum@likelion.net>
2021-11-19 15:27:19 +0900

    feat: 🎸 add webpack plugin override

--- packages/host/next.config.js
+++ packages/host/next.config.js
@@ -1,4 +1,33 @@
-/** @type {import('next').NextConfig} */
+const { NodeModuleFederation } = require("@telenko/node-mf");
+const ModuleFederationPlugin =
+  require("webpack").container.ModuleFederationPlugin;
+const packageJsonDeps = require("./package.json").dependencies;
+
 module.exports = {
   reactStrictMode: true,
-}
+  webpack: (config, options) => {
+    const { isServer } = options;
+
+    return {
+      ...config,
+      plugins: [
+        ...config.plugins,
+        new (isServer ? NodeModuleFederation : ModuleFederationPlugin)({
+          remotes: {},
+          shared: {
+            react: {
+              eager: true,
+              requiredVersion: packageJsonDeps["react"],
+              singleton: true
+            },
+            "react-dom": {
+              eager: true,
+              requiredVersion: packageJsonDeps["react-dom"],
+              singleton: true
+            }
+          }
+        })
+      ]
+    };
+  }
+};
